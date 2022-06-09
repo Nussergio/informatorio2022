952c019eab1e3fb733c1530ee68129cb55d89e8a
Sergio <nusss742@gmail.com>
2022-06-09 00:10:53 -0300

    actividad09/05

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Noticia from './componets/HolaMundo';
 
 function App() {
   return (
@@ -17,7 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <Noticia />
       </header>
+      <Noticia />
     </div>
   );
 }
---
+++ src/componets/HolaMundo.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Noticia() {
+  const nota = {
+    titulo: "React 2022",
+    descripcion: "Informatorio Chaco",
+};
+
+const {titulo, descripcion, } = nota;
+
+return (
+    <header className="header">
+        <h1>{titulo}</h1>
+    </header>
+    )
+};
+ export default Noticia;
